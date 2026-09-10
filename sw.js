a716a4e350ca5ea4e6edc20a8b4658f528cf573f
akatsukydeliverysushi-coder <akatsuky.deliverysushi@gmail.com>
2026-09-09 23:53:45 -0300

    Corrigir cache do PWA e atualizar comunicação com Companion

--- sw.js
+++ sw.js
@@ -1,4 +1,21 @@
-const CACHE='camera-monitor-v1';
+const CACHE='camera-monitor-v2';
 const ASSETS=['./','./index.html','./app.js','./style.css','./manifest.json'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+
+self.addEventListener('install',event=>{
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));
+});
+
+self.addEventListener('activate',event=>{
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()));
+});
+
+self.addEventListener('fetch',event=>{
+  const url=new URL(event.request.url);
+  if(url.origin===self.location.origin){
+    event.respondWith(fetch(event.request).then(response=>{
+      const copy=response.clone();
+      caches.open(CACHE).then(cache=>cache.put(event.request,copy)).catch(()=>{});
+      return response;
+    }).catch(()=>caches.match(event.request)));
+  }
+});
